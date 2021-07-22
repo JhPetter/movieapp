d5817ed5e207ad83714fd45cfa37afa0dab6d95e
Petter Jhunior Garcia Infante <petterjunior94@gmail.com>
2021-07-22 09:50:13 -0500

    design detail movie

---
+++ app/src/main/java/com/petter/movieapplication/ui/list/ListMovieActivity.kt
@@ -0,0 +1,11 @@
+package com.petter.movieapplication.ui.list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListMovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_movie)
+    }
+}
